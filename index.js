203d8d78ef9b98d18c0a3b28f057293255e73dc5
EchelonDev <raincloud@hotmail.com.tr>
2019-08-06 17:53:08 +0300

    class/function naming, fix redundant reassiging

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const { Plugin } = require('powercord/entities');
 
-var faces = ["(・`ω´・)", ";;w;;", "owo", "UwU", ">w<", "^w^"];
-function OwoifyText(v){
-    v = v.replace(/(?:r|l)/g, "w");
-    v = v.replace(/(?:R|L)/g, "W");
-    v = v.replace(/n([aeiou])/g, 'ny$1');
-    v = v.replace(/N([aeiou])/g, 'Ny$1');
-    v = v.replace(/N([AEIOU])/g, 'Ny$1');
-    v = v.replace(/ove/g, "uv");
-    v = v.replace(/\!+/g, " " + faces[Math.floor(Math.random() * faces.length)] + " ");
-	return v;
+let faces = ["(・`ω´・)", ";;w;;", "owo", "UwU", ">w<", "^w^"];
+function owoifyText(v){
+	return v
+    .replace(/(?:r|l)/g, "w")
+    .replace(/(?:R|L)/g, "W")
+    .replace(/(n)([aeiou])/gi, '$1y$2')
+    .replace(/ove/g, "uv")
+    .replace(/\!+/g, " " + faces[Math.floor(Math.random() * faces.length)] + " ");
 }
 
-module.exports = class owoify extends Plugin {
+module.exports = class Owoify extends Plugin {
   startPlugin () {
     this.registerCommand(
       'owo',
