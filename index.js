e288689cfcf2cee21e8cca95d975fcbf913a2278
Fezalion <raincloud@hotmail.com.tr>
2020-09-03 23:14:14 +0300

    Settings

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 const { Plugin } = require('powercord/entities');
+const { getModule, React } = require('powercord/webpack');
 const { inject, uninject} = require('powercord/injector');
-const { getModule } = require('powercord/webpack');
 
-let owoifierToggle = false;
+const Settings = require('./components/Settings');
+let owoifierAutoToggle = false;
 
-const faces = ["(・`ω´・)", ";;w;;", "owo", "UwU", ">w<", "^w^"];
-function owoifyText(v){
-	return v
-    .replace(/(?:r|l)/g, "w")
-    .replace(/(?:R|L)/g, "W")
-    .replace(/(n)([aeiou])/gi, '$1y$2')
-    .replace(/ove/g, "uv")
-	  .replace(/th/g, "ff")
-    .replace(/\!+/g, " " + faces[Math.floor(Math.random() * faces.length)] + " ");
-}
-
-module.exports = class Owoify extends Plugin {
-  constructor() {
-    super();
-  }  
-
+module.exports = class Owoify extends Plugin { 
   async startPlugin () {
     this.injection();
+    powercord.api.settings.registerSettings(this.entityID, {
+      category: this.entityID,
+      label: 'Owoifier',
+      render: (props) => React.createElement(Settings, {
+        main:this,
+        ...props
+      })
+    }); 
+
+    const getSetting = (setting, defaultValue) => this.settings.get(setting, defaultValue);
+
+    function owoifyText(v) {
+      let toggleFaces = getSetting('enableFaces');
+      let output = v
+        .replace(/(?:r|l)/g, "w")
+        .replace(/(?:R|L)/g, "W")
+        .replace(/(n)([aeiou])/gi, '$1y$2')
+        .replace(/ove/g, "uv")
+        .replace(/th/g, "ff");
+
+        if (toggleFaces) {
+          let faceList = getSetting('owoifierFaces');
+          output = output.replace(/\!+/g, " " + faceList[Math.floor(Math.random() * faceList.length)].name + " ");
+        }
+        return output;
+    }
 
     powercord.api.commands.registerCommand({
       command: 'owoify',
@@ -33,32 +45,33 @@ module.exports = class Owoify extends Plugin {
       command: 'owoifyauto',
       description: `owoify all of your messages`,
       executor: this.toggleAuto.bind(this)
-    });
-  }    
+    });    
+  }
   
   async injection() {
     const messageEvents = await getModule(["sendMessage"]);
     inject("owoifierSend", messageEvents, "sendMessage", function(args) {
-      let text = args[1].content;      
-      if(owoifierToggle)
+      if(owoifierAutoToggle) {
+        let text = args[1].content;
         text = owoifyText(text);
-
-      args[1].content = text;  
+        args[1].content = text;      
+      }      
       return args;  
     }, true);
   }
 
   pluginWillUnload() {
+    powercord.api.settings.unregisterSettings(this.entityID);
     uninject("owoifierSend"); 
     powercord.api.commands.unregisterCommand('owoifyauto');   
     powercord.api.commands.unregisterCommand('owoify');   
   }
 
   toggleAuto () {
-    owoifierToggle = !owoifierToggle;
+    owoifierAutoToggle = !owoifierAutoToggle;
     powercord.api.notices.sendToast('owoifyNotif', {
       header: `Owoify Auto Status`,
-      content: `${(owoifierToggle == true) ? 'Its weady to go >w<' : 'Ok chill we are back to normal again.'}`,
+      content: `${(owoifierAutoToggle == true) ? 'Its weady to go >w<' : 'Ok chill we are back to normal again.'}`,
       buttons: [{
         text: 'Dismiss',
         color: 'red',
