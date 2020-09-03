8fe97fa3513ea2cda9f31c508693ebbe677ff1e4
Fezalion <raincloud@hotmail.com.tr>
2020-09-03 23:17:00 +0300

    forgot stage

---
+++ components/Settings.jsx
@@ -0,0 +1,80 @@
+const { React } = require('powercord/webpack');
+const { FormTitle, Button } = require('powercord/components');
+const { SwitchItem, Category } = require('powercord/components/settings');
+
+const TextInputWithButton = require('./TextInputWithButton');
+
+let defFaces = [
+  { name: "(・`ω´・)" },
+  { name: ";;w;;" },
+  { name: "owo" },
+  { name: "UwU" },
+  { name: ">w<" },
+  { name: "^w^" }
+];
+
+let Faces = new Array();
+
+module.exports = class OwoifierSettings extends React.PureComponent {  
+    constructor(props) {
+        super(props)
+        this.state = { category1Opened: false }
+    }
+    handleAddNewFace () {
+      return Faces.concat({ name: null });
+    }
+    handleFacesListChange = (idx,newvalue) => {
+        const newFaces = Faces.map((face, sidx) => {
+          if (idx !== sidx) return face;
+          return { ...face, name: newvalue };
+        });
+        return newFaces;
+    };
+
+    handleRemoveFaces = (idx) => {
+      if(Faces.length > 1) {    
+        const newFaces = Faces.filter((obj,sidx) => sidx !== idx);
+        return newFaces;
+      }      
+    };
+
+    render() {
+        const { getSetting, toggleSetting, updateSetting } = this.props;
+        //Faces = defFaces; //is there a way to reset the settings ??
+        Faces = getSetting('owoifierFaces');
+        if(Faces.length < 1) { // if faces is empty, copy default faces to faces 
+          updateSetting('owoifierFaces',defFaces);
+          Faces = getSetting('owoifierFaces');
+        }        
+        return <> 
+        <FormTitle tag='h2'>Faces</FormTitle>
+        <SwitchItem
+        note="Enable this if you want your '!' to change to randomly selected cute faces listed below."
+        value={getSetting('enableFaces', true)}
+        onChange={() => toggleSetting('enableFaces')}
+      >
+        Faces
+      </SwitchItem>
+      <Category name="Faces" description="Configure which faces you want." opened={this.state.category1Opened} onChange={() => this.setState({ category1Opened: !this.state.category1Opened })}>
+      {Faces.map((face, idx) => (
+          <div>
+            <TextInputWithButton
+              type="text"
+              placeholder={`Face #${idx + 1}`}
+              defaultValue={face.name}
+              onChange={val => {updateSetting('owoifierFaces', this.handleFacesListChange(idx,val))}}
+              onClick={() => {updateSetting('owoifierFaces', this.handleRemoveFaces(idx))}}
+              text="Remove"
+              icon="fal fa-minus"
+            />
+          </div>
+        ))}
+        <Button
+          onClick={() => {updateSetting('owoifierFaces', this.handleAddNewFace())}}
+        >
+          Add Face
+        </Button>           
+        </Category>
+        </>;
+    }
+}
